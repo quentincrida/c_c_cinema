e5aa90bd995ff29fc402981ffff6220e06cdb37d
Quentin Crida <qcrida@gmail.com>
2019-11-22 15:32:30 +0000

    customer2 added and saved

--- console.rb
+++ console.rb
@@ -7,3 +7,17 @@ Customer.delete_all()
 
 
 customer1 = Customer.new({ 'name' => 'Quentin Crida', 'funds' => 50})
+customer1.save()
+customer2 = Customer.new({'name' => 'Este Visser', 'funds' => 60})
+customer2.save()
+
+
+
+
+
+
+
+
+
+binding.pry
+nil
