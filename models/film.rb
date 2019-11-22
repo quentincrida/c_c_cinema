e899efbaff697f012dd9a9b3e0156a644c0d7b42
Quentin Crida <qcrida@gmail.com>
2019-11-22 17:08:48 +0000

    film update tested

--- models/film.rb
+++ models/film.rb
@@ -27,6 +27,13 @@ class Film
     SqlRunner.run(sql)
   end
 
+  def update()
+    sql = "UPDATE films SET (title, price)
+    = ($1, $2) WHERE id = $3"
+    values = [@title, @price, @id]
+    SqlRunner.run(sql, values)
+  end
+
 
 
 end
